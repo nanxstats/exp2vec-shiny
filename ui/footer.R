f728151aebd8cb7398393d6aadf235301c4c4e56
Nan Xiao <me@nanx.me>
2022-08-03 20:37:22 -0400

    Add tissue name and repo link

--- ui/footer.R
+++ ui/footer.R
@@ -6,6 +6,7 @@ tags$footer(
       hr(),
       includeCSS("www/custom.css"),
       p(paste("exp2vec-shiny", as.vector(read.dcf("DESCRIPTION", fields = "Version"))), class = "text-muted"),
+      p("Embedding ", a("trained", href = "https://github.com/nanxstats/exp2vec", target = "_blank"), "on pancreas data from GTEx v8", class = "text-muted"),
       p("Built by ", a("Nan Xiao", href = "https://nanx.me/", target = "_blank"), class = "text-muted")
     )
   )
